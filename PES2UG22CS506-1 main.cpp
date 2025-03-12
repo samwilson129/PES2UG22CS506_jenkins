e710db74f2f06b818dbd0b8a6374104eea217853
sanket <94269039+samwilson129@users.noreply.github.com>
2025-03-12 17:38:06 +0530

    Delete PES2UG22CS506-1 main.cpp

--- PES2UG22CS506-1 main.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from Jenkins Pipeline!" << std::endl;
-    std::cout << "This is a test program for CI/CD demonstration" << std::endl;
-    return 0;
-}
