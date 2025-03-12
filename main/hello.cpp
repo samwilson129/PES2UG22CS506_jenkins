514f8a32ca320dcb49f1506ea08de4b9c2642923
sanket <94269039+samwilson129@users.noreply.github.com>
2025-03-12 17:15:00 +0530

    commit hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES2UG22CS506 \n";
+  cout << "PES2UG22CS506 SRN \n";
   return 0;
 }
